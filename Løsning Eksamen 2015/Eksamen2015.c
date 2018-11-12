1eee490aab04bee03189d242c535115e538f6824
Anders Madsen <amads15@student.aau.dk>
2018-11-12 13:32:11 +0100

    smukkeseret det

--- Løsning Eksamen 2015/Eksamen2015.c
+++ Løsning Eksamen 2015/Eksamen2015.c
@@ -32,7 +32,7 @@ typedef struct{
 }match_t;
 
 
-
+void solve_all_tasks();
 void get_match_data(match_t *output); 
 void opgave_1(match_t *input);                        /*Find matches with/more 7 goals*/
 void opgave_2(match_t *input);                        /*Find the round with highest score*/
@@ -46,61 +46,34 @@ void opgave_6(match_t *input, team_x *all_teams);     /*Sorting the teams by poi
 int comp(const void *A, const void *B);               /*Sorting function for "opgave_6"*/
 
 
-main(void){
-  match_t match_arr[200];
-  team_x team_arr[12];    
-  int i;                                              /*"i" is being used throughout this program as an variable for loop*/                                            
-  double spectators_season;                           
-  int boring_team=0;  
-  
-  get_match_data(match_arr);
-  opgave_1(match_arr);
-  opgave_2(match_arr);
-  get_team_names(match_arr, team_arr);
-  opgave_3(match_arr, team_arr);
-    printf("Opgave 3:\n");
-    
-    for(i=0; i<=NUMBER_OF_TEAMS; i++){
-      if(team_arr[i].homewin<team_arr[i].awaywin){
-      printf("%s only won %d at home but won %d away\n", team_arr[i].name, team_arr[i].homewin, team_arr[i].awaywin); 
-      }
-    }
-  opgave_4(match_arr, team_arr);
-  printf("Opgave 4:\n");
-    spectators_season=team_arr[10].spectators;         /*Team_arr[10] equals to FCK - the biggest team in Denmark.
-                                                         To make sure, spectators_season was lower than [i]spectators*/
-    for(i=0; i<=NUMBER_OF_TEAMS; i++){
-      if(team_arr[i].spectators<spectators_season){
-        spectators_season=team_arr[i].spectators;       
-        boring_team=i;                                  /*used to save the teams number*/
-      }
-    }  
-    printf("%s was the team who had fewest spectators throughout the season\nwith only %.3lf spectators going to there matches.\n", team_arr[boring_team].name, spectators_season);
-  
-  opgave_5(match_arr);
-  opgave_6(match_arr, team_arr);
-    
-    printf("Opgave 6:\n");
-    
-    printf("Hold\tK\tV\tU\tT\t+/-\tMf\tMi\tP\n");
-    for(i=0; i<=NUMBER_OF_TEAMS; i++){
-
-      printf("%s\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\n", team_arr[i].name, team_arr[i].games_played, (team_arr[i].homewin+team_arr[i].awaywin), team_arr[i].draws, 
-                                                    (team_arr[i].homeloss+team_arr[i].awayloss), team_arr[i].goal_diff, team_arr[i].goals_scored, team_arr[i].goals_conceded, team_arr[i].total_points);
-    }
-  fclose(input_file);
-  return 0;
+int main(void){
+    solve_all_tasks();
+    return 0;
 }
+
+void solve_all_tasks(){
+    match_t match_arr[200];
+    team_x team_arr[12];    
+    
+    get_match_data(match_arr);
+    opgave_1(match_arr);
+    opgave_2(match_arr);
+    get_team_names(match_arr, team_arr);
+    opgave_3(match_arr, team_arr);
+    opgave_4(match_arr, team_arr);
+    opgave_5(match_arr);
+    opgave_6(match_arr, team_arr);
+}
+
 
 void get_match_data(match_t *output){      
     static const char filename[] = "kampresultater.txt";
     FILE *file = fopen(filename, "r");
-    int i = 0;
     char temp[60];                                        /*Used for saving info from fgets temporaily*/
     int i=0,j=1;                                          /*Loop variables*/
   
   
-    while((fgets(temp, 5000, input)) != NULL){
+    while((fgets(temp, 5000, file)) != NULL){
         if(strlen(temp)>5){                                 /*If the length of temp is lower than 5, its a blank line, and it wont get saved*/
             sscanf(temp,"%s %d/%d %lf %s - %s %d - %d %lf", output[i].weekday, &output[i].day, 
                                                         &output[i].month, &output[i].time, output[i].hometeam, output[i].awayteam, 
@@ -112,6 +85,7 @@ void get_match_data(match_t *output){
             j++;                                              /*If its lower than 5 it will count up the round number*/
         }
     }
+    fclose(file);
 }    
 
 void opgave_1(match_t *input){
@@ -190,16 +164,34 @@ void opgave_3(match_t *input, team_x *all_teams){
             all_teams[home].homeloss++;
         }
     }    
+    printf("Opgave 3:\n");    
+    for(i=0; i<=NUMBER_OF_TEAMS; i++){
+        if(all_teams[i].homewin<all_teams[i].awaywin){
+            printf("%s only won %d at home but won %d away\n", all_teams[i].name, all_teams[i].homewin, all_teams[i].awaywin); 
+        }
+    }
 }
 
 void opgave_4(match_t *input, team_x *all_teams){
     int i;
     int home;
-    
+    int spectators_season;
+    int boring_team = 0;
     for(i=0; i<=MAX_MATCHES; i++){                        /*Counting all the spectators for all matches*/
         home = team_id(input[i].hometeam, all_teams);
         all_teams[home].spectators = input[i].num_spec+all_teams[home].spectators;  /*Adding the amount of spectators to hometeam current amount*/
     }
+    printf("Opgave 4:\n");
+    spectators_season = all_teams[10].spectators;         /*all_teams[10] equals to FCK - the biggest team in Denmark.
+                                                         To make sure, spectators_season was lower than [i]spectators*/
+    for(i=0; i<=NUMBER_OF_TEAMS; i++){
+      if(all_teams[i].spectators<spectators_season){
+        spectators_season=all_teams[i].spectators;       
+        boring_team=i;                                  /*used to save the teams number*/
+      }
+    }  
+    printf("%s was the team who had fewest spectators throughout the season\nwith only %d spectators going to there matches.\n", all_teams[boring_team].name, spectators_season);
+  
 }
 
 void opgave_5(match_t *input){                        
@@ -253,7 +245,15 @@ void opgave_6(match_t *input, team_x *all_teams){
         all_teams[i].total_points = ((all_teams[i].homewin+all_teams[i].awaywin)*WIN)+(all_teams[i].draws); /*Counting total points for team*/
         all_teams[i].goal_diff = all_teams[i].goals_scored - all_teams[i].goals_conceded;                   /*Counting goal difference for team*/
     }
-    qsort(all_teams, 12 , sizeof all_teams[0], comp);                                                     /*Sorting teams by points*/
+    qsort(all_teams, 12 , sizeof all_teams[0], comp);                                                  /*Sorting teams by points*/
+    
+    printf("Opgave 6:\n");
+    printf("Hold\tK\tV\tU\tT\t+/-\tMf\tMi\tP\n");
+    
+    for(i=0; i<=NUMBER_OF_TEAMS; i++){
+        printf("%s\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\n", all_teams[i].name, all_teams[i].games_played, (all_teams[i].homewin+all_teams[i].awaywin), all_teams[i].draws, 
+                                                        (all_teams[i].homeloss+all_teams[i].awayloss), all_teams[i].goal_diff, all_teams[i].goals_scored, all_teams[i].goals_conceded, all_teams[i].total_points);
+    }
 }
 
 int comp(const void *A, const void *B){
